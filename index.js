1489131b4a17b35cbb1611daf6dafb730d723951
rrohitmishraa <rohit255141@gmail.com>
2024-10-20 04:51:20 +0530

    1.5

--- index.js
+++ index.js
@@ -22,6 +22,15 @@ app.post("/", (req, res) => {
   const userr = new UserModel({ user, pass });
   userr.save();
   res.send("Done");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+  // res.setHeader("Content-Type", "application/json;charset=utf-8"); // Opening this comment will cause problems
 });
 
 app.listen(process.env.PORT, () => {
